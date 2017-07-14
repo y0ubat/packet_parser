634e2e11b62cd1aa11560516dcaa10342515ec66
y0ubat <y0u_bat@naver.com>
2017-07-14 11:59:08 -0700

    ip header add

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <pcap.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 
 #pragma pack(push,1)
@@ -8,6 +10,52 @@ struct packet_eth
     u_int8_t daddr[6];
     u_int8_t saddr[6];
     u_int16_t type;
+};
+
+struct packet_ip
+{
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    unsigned int ip_hl:4;    /* header length */
+    unsigned int ip_v:4;    /* version */
+#endif
+#if __BYTE_ORDER == __BIG_ENDIAN
+    unsigned int ip_v:4;    /* version */
+    unsigned int ip_hl:4;    /* header length */
+#endif
+    u_int8_t ip_tos;      /* type of service */
+    u_short ip_len;      /* total length */
+    u_short ip_id;      /* identification */
+    u_short ip_off;      /* fragment offset field */
+#define  IP_RF 0x8000      /* reserved fragment flag */
+#define  IP_DF 0x4000      /* dont fragment flag */
+#define  IP_MF 0x2000      /* more fragments flag */
+#define  IP_OFFMASK 0x1fff    /* mask for fragmenting bits */
+    u_int8_t ip_ttl;      /* time to live */
+    u_int8_t ip_p;      /* protocol */
+    u_short ip_sum;      /* checksum */
+    struct in_addr ip_src, ip_dst;  /* source and dest address */
+};
+
+struct packet_tcp
+{
+    u_short sport;
+    u_short dport;
+    u_int16_t sq_num;
+    u_int16_t ack_num;
+    u_short data_offset:4;
+    u_short reserved:3;
+    unsigned char fin:1;
+    unsigned char syn:1;
+    unsigned char rst:1;
+    unsigned char psh:1;
+    unsigned char ack:1;
+    unsigned char urg:1;
+    unsigned char ecn:1;
+    unsigned char cwr:1;
+    unsigned short window;
+    unsigned short checksum;
+    unsigned short urgent_pointer;
+
 };
 
 #pragma pack(pop)
@@ -25,6 +73,8 @@ int main(int argc, char *argv[])
     struct pcap_pkthdr header;	/* The header that pcap gives us */
     const u_char *packet;		/* The actual packet */
     struct packet_eth *eth;
+    struct packet_ip *ip;
+    struct packet_tcp *tcp;
 
     /* Define the device */
     dev = pcap_lookupdev(errbuf);
@@ -57,7 +107,7 @@ int main(int argc, char *argv[])
     while(1)
     {
         pcap_next_ex(handle, &header,&packet);
-        eth = (struct ether_header*)packet;
+        eth = (struct packet_eth*)packet;
         printf("\n\n\n");
 
         printf("eht destnation: ");
@@ -72,13 +122,30 @@ int main(int argc, char *argv[])
 
         switch(eth->type)
         {
-            case 0x08:
-                printf("aaaa\n");
-                break;
+        case 0x08:
+            packet += sizeof(struct packet_eth);
+            ip = (struct packet_ip*)packet;
+            printf("dip : %s\n",inet_ntoa(ip->ip_dst));
+            printf("sip : %s\n",inet_ntoa(ip->ip_src));
+
+            if(ip->ip_p == 6)
+            {
+                packet += sizeof(struct packet_ip);
+                tcp = (struct packet_tcp*)packet;
+                printf("aa\n");
+                //printf("dport : %d\n",tcp->dport);
+                //printf("sport : %d\n",tcp->sport);
+            }
+
+            break;
+
 
         }
 
+
     }
+
+
     pcap_close(handle);
     return(0);
 }
