4cb1f5e4b32f96ec31a7cb6e161d43fd6f373a05
y0ubat <y0u_bat@naver.com>
2017-07-17 11:26:01 -0700

    completesudo git add *

--- main.c
+++ main.c
@@ -27,22 +27,12 @@ struct packet_tcp{
 
 struct packet_ip
 {
-#if __BYTE_ORDER == __LITTLE_ENDIAN
     unsigned int ip_hl:4;    /* header length */
     unsigned int ip_v:4;    /* version */
-#endif
-#if __BYTE_ORDER == __BIG_ENDIAN
-    unsigned int ip_v:4;    /* version */
-    unsigned int ip_hl:4;    /* header length */
-#endif
     u_int8_t ip_tos;      /* type of service */
     u_short ip_len;      /* total length */
     u_short ip_id;      /* identification */
     u_short ip_off;      /* fragment offset field */
-#define  IP_RF 0x8000      /* reserved fragment flag */
-#define  IP_DF 0x4000      /* dont fragment flag */
-#define  IP_MF 0x2000      /* more fragments flag */
-#define  IP_OFFMASK 0x1fff    /* mask for fragmenting bits */
     u_int8_t ip_ttl;      /* time to live */
     u_int8_t ip_p;      /* protocol */
     u_short ip_sum;      /* checksum */
@@ -103,7 +93,6 @@ int main(int argc, char *argv[])
     while(1)
     {
         int res;
-        int data_len;
         res = pcap_next_ex(handle, &header,&packet);
         // data_len = header.len - (sizeof(struct packet_tcp) + sizeof(struct packet_ip) +sizeof(struct packet_eth));
 
